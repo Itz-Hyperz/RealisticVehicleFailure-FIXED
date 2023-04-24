1934e98e33b7a62e9ff0279b8d2f9b872d98bad2
Itz-Hyperz <logan@bosssoftware.net>
2023-04-24 19:42:08 -0400

    Update config.lua

--- config.lua
+++ config.lua
@@ -88,6 +88,8 @@ cfg = {
 
 cfg = {
 
+	repairTimeoutMS = 5000,                     -- How long it takes for the mechanic to look at your vehicle before repairing it. Default = 5000 (5 seconds)
+
 	deformationMultiplier = -1,					-- How much should the vehicle visually deform from a collision. Range 0.0 to 10.0 Where 0.0 is no deformation and 10.0 is 10x deformation. -1 = Don't touch
 	deformationExponent = 1.0,					-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
 	collisionDamageExponent = 1.0,				-- How much should the handling file deformation setting be compressed toward 1.0. (Make cars more similar). A value of 1=no change. Lower values will compress more, values above 1 it will expand. Dont set to zero or negative.
